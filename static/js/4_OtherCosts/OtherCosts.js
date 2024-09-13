51518517a2186841a203a6ce9282de8d74120adf
pistirozsa <pisti.rzsa@gmail.com>
2024-09-13 09:11:19 +0200

    Refresh data

--- static/js/4_OtherCosts/OtherCosts.js
+++ static/js/4_OtherCosts/OtherCosts.js
@@ -3,7 +3,7 @@ function drawOtherCostCharts() {
     const summaryOtherCost = summarizeJSON(other,["GYR-Cikknév","GYR-Cikkszám",""]);
     drawBarchart(summaryOtherCost,"featureName","summarizedValue","#other_bar_chart",5,"featureName",1)
     fillTable(other, "#other_table");
-    sumKeys(other,["Szerszám, alkatrész (5121)","Áramdíj költség (513)","Vízdij (514)","Gázdíj költsége (515)","Bérleti díj (522)","Karbantartás költsége (524)","Értékcsökkenés költsége (57)","Bérköltség ktgh(54)","Bérjárulékok ktgh(56)","Munkaerő kölcsönzés bér ktg.(5298)","Munkaerő kölcsönzés jutalék ktg (5298)"],"#all_other");
+    sumKeys(other,["Szerszám, alkatrész(5121)","Áramdíj költség(513)","Vízdíj(514)","Gázdíj költsége(515)","Bérleti díj(522)","Karbantartás költsége(524)","Értékcsökkenés költsége(57)","Bérköltség ktgh(54)","Bérjárulékok ktgh(56)","Munkaerő kölcsönzés bér ktg.(5298)","Munkaerő kölcsönzés jutalék ktg.(5298)"],"#all_other");
     d3.select("#other_filter_no").on("input", () => filterData('#other_filter_no',"#other_filter_name", "#other_table",other,"GYR-Cikkszám","GYR-Cikknév"));
     d3.select("#other_filter_name").on("input", () => filterData('#other_filter_no',"#other_filter_name", "#other_table",other,"GYR-Cikkszám","GYR-Cikknév"));
 }
